bd30335d6bdcdc20dc5b16f2da909551ad2c623f
kartik ramvani <kartik.ramvani367@gmail.com>
2025-08-07 01:13:48 +0530

    add changes

--- script.js
+++ script.js
@@ -3,24 +3,33 @@ const Username = document.querySelector(".Username");
 const X_Acc_Link = document.querySelector(".X_Acc_Link");
 const Bio = document.querySelector(".Bio_para");
 const fa_x_twitter = document.querySelector(".fa-x-twitter");
-// const Btn = document.querySelector(".getUserBtn");
-
+const Btn = document.querySelector(".getUserBtn");
+const login = document.querySelector(".login");
+const Followers = document.querySelector(".Followers");
+const Following = document.querySelector(".Following");
 const GitAPI = "https://api.github.com/users/KARTIK78O";
 
+// Starting Page Or Jab Bhi Load Hoga
 function ResetAll() {
   user_avatar.src =
-    "https://tse1.mm.bing.net/th/id/OIP.hfF9W17CHoBL-nukZBRptwHaF9?rs=1&pid=ImgDetMain&o=7&rm=3";
+    "https://dpemoji.com/wp-content/uploads/2023/02/cartoon-dp-for-boys-14.png";
   user_avatar.alt = "dummy DP";
-
   Username.textContent = "Username";
-  X_Acc_Link.textContent = "https://twitter.com/FakeID";
+  login.textContent = "Login";
+  Followers.textContent = "Followers";
+  Following.textContent = "Following";
+  X_Acc_Link.textContent = "https://twitter.com/fakeAccount";
   Bio.textContent = "Bio";
 }
 
-ResetAll();
+ResetAll(); // Calling the ResetAll Function [Starting Page Or Jab Bhi Load Hoga]
 
-document.querySelector(".getUserBtn").addEventListener("click", getdata);
+//====================================================================================|
+
 // Jaise hi btn par click hoga getdata Call Hoga Aur Data Inject ho Jaega
+Btn.addEventListener("click", getdata);
+
+//====================================================================================|
 
 // getting Data - Data values and Key
 async function getdata() {
@@ -32,6 +41,9 @@ async function getdata() {
   user_avatar.src = Data.avatar_url;
   user_avatar.alt = "Kartik Ramvani DP";
   Username.textContent = Data.name;
-  X_Acc_Link.innerHTML = Data.twitter_username ;
+  login.textContent = Data.login;
+  Followers.textContent = `Followers: ${Data.followers}`;
+  Following.textContent = `Following: ${Data.following}`;
+  X_Acc_Link.innerHTML = Data.twitter_username;
   Bio.textContent = Data.bio;
 }
