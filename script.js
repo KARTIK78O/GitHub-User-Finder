e89ed8029b7cb4b148e6922dc1df235acec6811d
kartik ramvani <kartik.ramvani367@gmail.com>
2025-08-06 23:21:05 +0530

    add changes

--- script.js
+++ script.js
@@ -8,21 +8,22 @@ const GitAPI = "https://api.github.com/users/KARTIK78O";
 
 function ResetAll() {
   if (user_avatar) {
-    user_avatar.src = "";
+    user_avatar.src = "https://tse1.mm.bing.net/th/id/OIP.hfF9W17CHoBL-nukZBRptwHaF9?rs=1&pid=ImgDetMain&o=7&rm=3";
     user_avatar.alt = "";
   }
 
-  if (Username) Username.textContent = "";
-  if (X_Acc_Link) X_Acc_Link.href = "#";
-  if (Bio) Bio.textContent = "";
+  if (Username) Username.textContent = "Username";
+  if (X_Acc_Link) X_Acc_Link.textContent = "https://twitter.com/FakeID";
+  if (Bio) Bio.textContent = "Bio";
 }
 
 ResetAll();
+
 // getting Data - Data values and Key
 async function getdata() {
   let response = await fetch(GitAPI);  // Api Fetch Karega Jab Tak data na Aa gye await se wait krega
-  let Data = await response.json();   // API ke Data Ko Json me Convert karega
-  console.log(response);             // Printing response
+  let Data = await response.json();   // API ke Data Ko Json me Convert karega [Aur ye hi Actual me Data hai Jo hame Chaiye]
+  console.log(Data);             // Printing response
 }
 
 getdata(); // Calling Function
